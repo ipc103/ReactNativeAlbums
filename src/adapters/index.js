44b203e412327714d9892ecc15d55479b9e58802
ipc103 <iancandysemail@gmail.com>
2017-04-01 14:02:51 -0400

    swap demo api for spotify

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -1,6 +1,18 @@
 import { get } from './requests'
-const BASE_URL = 'https://rallycoding.herokuapp.com/api'
+const BASE_URL = 'https://api.spotify.com/v1/albums/'
 
 export function fetchAlbums(){
-  return get(`${BASE_URL}/music_albums`)
+  return get(`${BASE_URL}?ids=41MnTivkwTO3UUJ8DrqEJJ,6JWc4iAiJ9FjyK0B59ABb4,6UXCm6bOO4gFlDQZV5yL37`)
+    .then(formatAlbums)
 }
+
+const formatAlbums = (data) => data.albums.map(formatAlbum)
+
+const formatAlbum = (album) => (
+  {
+    title: album.name,
+    thumbnail_image: album.images[2].url,
+    image: album.images[0].url,
+    url: album.external_urls.spotify
+  }
+)
